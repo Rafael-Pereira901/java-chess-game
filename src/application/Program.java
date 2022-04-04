6eaffae1076a9065c29e80ce722ad57c77c84757
Rafael Pereira <rafaelpereira901@gmail.com>
2022-04-04 10:44:41 -0300

    Position Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("helo");
+		
+		Position p = new Position(2, 3);
+		
+		System.out.println(p);
+		
 	}
 
 }
